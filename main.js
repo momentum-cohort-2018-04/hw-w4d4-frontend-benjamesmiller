d30371a41b4e39a2712373db310f6825c865fc9c
benjamesmiller <benjamesmiller@gmail.com>
2018-05-17 15:07:21 -0400

    Staring Ajax requests

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-import request from "superagent"
+import request from "superagent";
+import $ from "jQuery";
+
+let song = []
+
+function apiURL(artist) {
+    return `https://itunes.apple.com/lookup${artist}`
+}
+
+function getandshowResults () {
+    request
+    .get(apiURL('artist'))
+    .then(Response => {
+        
+    })
+}
+
+
+
+$(document).ready(function() {
+
+    $(document).on('click', '.button', function (event) {
+        const artistID = event.target.dataset.artistID
+        getandshowResults(artistID)  
+    })
+}
+)
+
 
 
