aef6328dc9565857ff4fe8ca8b830db0a5f8da2c
benjamesmiller <benjamesmiller@gmail.com>
2018-05-18 09:32:26 -0400

    Gets data from iTunes

--- main.js
+++ main.js
@@ -4,36 +4,41 @@ window.$ = $
 let song = []
 
 function apiURL(artist) {
-    return `https://itunes.apple.com/search?term=${artist}&attribute=artistterm`
+    return `https://itunes.apple.com/search?term=${artist}&media=music`
 }
 
-function getandshowResults () {
-    request
-    .get(apiURL(artist))
-    .then(response => { 
-        let parsedrep = JSON.parse(response.text) 
-        console.log(parsedrep)
-        artisttoHTML(parsedrep) 
-    })
-}
-
-function artisttoHTML (response) {
-    for (var i=1; i<response.length; i++) {
-        const img = i.response
-        console.log(img)
-    return `<img src="${img}">`
-}
-}
 
 $(document).ready(function() {
 
-    const artist = $(".search").val()
+    $(document).on('')
+    
 
     $(document).on('click', '.button', function (event) {
         const artistID = event.target.dataset.artistID
+        let artist = $(".search").val()
         getandshowResults(artistID)  
         
     })
+
+    function getandshowResults () {
+        let artist = $(".search").val()
+        request
+        .get(apiURL(artist))
+        .then(response => { 
+            let parsedrep = JSON.parse(response.text) 
+            console.log(parsedrep)
+            artisttoHTML(parsedrep) 
+        })
+    }
+    
+    function artisttoHTML (response) {
+        for (var i=1; i<response.length; i++) {
+            const img = i.response
+            console.log(img)
+        return `<img src="${img}">`
+    }
+    }
+    
 }
 )
 
